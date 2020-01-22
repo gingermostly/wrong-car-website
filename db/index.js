70a6c742927fb70b3b23dd36aadeb08f4f948e18
gingermostly <gingermostly@gmail.com>
2020-01-21 19:14:01 -0500

    create Schema for gigography database

--- db/index.js
+++ db/index.js
@@ -3,8 +3,35 @@ const dotenv = require('dotenv');
 dotenv.config();
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('connected, yay!')
 });
+
+const gigSchema = {
+    date: Date,
+    city: String,
+    state: String,
+    country: String,
+    venue: String,
+    details: String
+}
+
+const Gig = mongoose.model('Gig', gigSchema);
+
+let testGig = new Gig({
+    date: '06/12/2016',
+    city: 'Toronto',
+    state: 'ON',
+    country: 'Canada',
+    venue: 'Woodbine Park',
+    details: 'Bestival Music Festival - with the Cure'
+});
+
+testGig.save((err, data)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log(data)
+})
