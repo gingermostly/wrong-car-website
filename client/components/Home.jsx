f0f9c2397586761b39c6827bd3617a44f0d2e7bf
gingermostly <gingermostly@gmail.com>
2020-11-22 12:51:36 -0500

    truncate article text on home page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,31 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+const truncateStr = (str, num) => {
+  if (str.length <= num) {
+    return str
+  }
+  return str.slice(0, num) + '...'
+}
+
+const ArticleButton = styled.button`
+  background: #c71742;
+  border: 0;
+  margin-top: 1em;
+  padding: 8px;
+  border-radius: 2px;
+  color: #fff;
+  font: inherit;
+  a {
+    color: #fff;
+    font: inherit;
+    text-decoration: none;
+  }
+  a:hover {
+    cursor: pointer
+  }
+`
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -21,7 +46,14 @@ class Home extends React.Component {
     }
   render() {
       return this.state.data.map(entry => {
-        return <Link to={`/articles/${entry.article_id}`}><div>{entry.title}</div></Link>
+        return (
+          <div>
+            <h2>{entry.title}</h2>
+            <span>{entry.published_at}</span>
+            <div>{truncateStr(entry.content, 200)}</div>
+              <ArticleButton><Link to={`/articles/${entry.article_id}`}>read more</Link></ArticleButton>   
+          </div>
+          )
       })
   }
 }
